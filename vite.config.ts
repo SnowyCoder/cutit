db4cc312ee553199d91b686aec763f891db57353
Rossi Lorenzo <snowycoder@gmail.com>
2023-08-01 20:16:11 +0200

    Add license, recommended extensions and vite base

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(), tailwindcss()
+    vue()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: '/cutit/'
 })
